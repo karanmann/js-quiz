c05f01a1075d80c4ad5720c9cd5e5b485a96cb8b
Karan Mann <karan.mann@outlook.com>
2021-11-11 10:19:40 +0100

    Your score added

--- scripts/end.js
+++ scripts/end.js
@@ -7,7 +7,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const maxHighScores = 5;
 
-finalScore.innerText = mostRecentScore;
+finalScore.innerText += mostRecentScore;
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
